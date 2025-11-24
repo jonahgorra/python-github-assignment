357ffae511afda2ffc1a7c909158396d6b5d1e8e
Jonah Gorra <jgorra@usf.edu>
2025-11-23 23:35:22 -0500

    Add formatted output showing weekly estimate

--- app.py
+++ app.py
@@ -2,3 +2,4 @@ print("Welcome to my Python program!")
 hours_input = input("How many hours did you study today? ")
 daily_hours = float(hours_input)
 weekly_hours = daily_hours * 7
+print(f"You are on track to study approximately {weekly_hours} hours this week.")
